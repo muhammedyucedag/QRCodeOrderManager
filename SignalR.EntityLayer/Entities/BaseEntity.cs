742b0dab6fbf38e3f147e88ef1faaad6132550f1
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-05-25 23:25:21 +0300

    Base entitiy için Description eklendi

--- SignalR.EntityLayer/Entities/BaseEntity.cs
+++ SignalR.EntityLayer/Entities/BaseEntity.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel;
+
 namespace SignalR.Entity.Entities;
 
 public class BaseEntity
 {
+    [Description("Benzersiz Kimlik")]
     public int Id { get; set; }
+    
+    [Description("Oluşturma Tarihi")]
     public DateTime CreatedDate { get; set; }
+    
+    [Description("Güncelleme Tarihi")]
     public DateTime UpdateDate { get; set; }
 }

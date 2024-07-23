9c438c0eeee1775dc3c94cea713310776ad4df96
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-07-23 21:39:53 +0300

    indirim tutarı için güncelleme tamamlandı

--- QRCodeOrderManager.Application/Features/Commands/Discount/Update/UpdateDiscountCommandHandler.cs
+++ QRCodeOrderManager.Application/Features/Commands/Discount/Update/UpdateDiscountCommandHandler.cs
@@ -1,11 +1,20 @@
 using MediatR;
+using QRCodeOrderManager.Application.Abstractions.Services;
 
 namespace QRCodeOrderManager.Application.Features.Commands.Discount.Update;
 
 public class UpdateDiscountCommandHandler : IRequestHandler<UpdateDiscountCommand, UpdateDiscountCommandResponse>
 {
-    public Task<UpdateDiscountCommandResponse> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+    private readonly IDiscountService _discountService;
+
+    public UpdateDiscountCommandHandler(IDiscountService discountService)
     {
-        throw new NotImplementedException();
+        _discountService = discountService;
+    }
+    
+    public async Task<UpdateDiscountCommandResponse> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+    {
+        await _discountService.UpdateAsync(request);
+        return new();
     }
 }

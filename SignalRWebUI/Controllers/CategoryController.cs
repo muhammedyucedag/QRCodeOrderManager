4ba0129627615fc627bfe927e339cb319e67ebed
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-08-26 20:51:01 +0300

    kategori alanlarının tamamlanması

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -45,8 +45,56 @@ namespace SignalRWebUI.Controllers
             }
 
             return View();
+        }
 
-        } 
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5035/api/categories/{id}");
+            
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5035/api/categories/{id}");
+            
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<CategoryDto>(jsonData);
+                
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(CategoryDto categoryDto)
+        {
+            var client = httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(categoryDto);
+            
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            
+            var responseMessage = await client.PutAsync("http://localhost:5035/api/categories/UpdateCategory/", stringContent);
+            
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
 
     }
 }
